73a09c23c6ed43cf568921a61a45915b6d9b2d35
Mohamed Ahmed <a.mohamed.cali@gmail.com>
2020-06-17 23:40:13 +0200

    feat: add react router

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { handleAddTweet } from '../actions/tweets'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 
 class NewTweet extends Component {
     state = {
         text: '',
+        toHome: false,
     }
 
     handleChange = (e) => {
@@ -27,14 +29,17 @@ class NewTweet extends Component {
         console.log('New Tweet: ', text)
 
         this.setState(() => ({
-            text: ''
+            text: '',
+            toHome: id ? false : true,
         }))
     }
 
     render() {
-        const { text } = this.state
+        const { text, toHome } = this.state
 
-        {/* todo: Redirect to / if submitted  */}
+        if (toHome === true) {
+            return <Redirect to='/' />
+        }
 
         const tweetLeft = 280 - text.length
 
